9eef8bf4060c184723950cba30e2c4b88852504f
pi342us-g <ndubipius96@gmail.com>
2025-06-23 21:18:12 +0300

    first node

---
+++ index.js
@@ -0,0 +1,15 @@
+// import chalk dependencies
+const chalk = require("chalk")
+console.log(chalk.red("Hello there"))
+console.log(chalk.blue("This is another line of code"))
+console.log(chalk.bgYellow("The server is already running"))
+console.log()
+
+
+// how to create node application   npm init -y
+// how to install packages either globally or locally
+// how to configure the start script out of the package.json file
+
+
+// create a new node project that contain a function which find the sum of three numbers
+// inside  installchalk package and use it on out put
